ed08a50bac3520c3f3e8fbee893598428232bc3e
Andrewkha <chernyshov.andrey@gmail.com>
2017-01-15 22:05:31 +0300

    Initial commit

---
+++ config/db.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'mysql:host=localhost;dbname=sportforecast',
+    'username' => 'sfadmin',
+    'password' => 'sfadmin!@',
+    'charset' => 'utf8',
+    'tablePrefix' => 'sf_'
+];
