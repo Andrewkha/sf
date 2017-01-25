9708e20d24f6dc39aaa3af514979e669d8e231d9
Andrewkha <chernyshov.andrey@gmail.com>
2017-01-25 18:02:45 +0300

    Country CRUD and model generated

---
+++ modules/admin/models/Country.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace app\modules\admin\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "{{%country}}".
+ *
+ * @property integer $id
+ * @property string $country
+ *
+ * @property Team[] $teams
+ * @property Tournament[] $tournaments
+ */
+class Country extends \yii\db\ActiveRecord
+{
+    /**
+     * @inheritdoc
+     */
+    public static function tableName()
+    {
+        return '{{%country}}';
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function rules()
+    {
+        return [
+            [['country'], 'required'],
+            [['country'], 'string', 'max' => 50],
+        ];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'country' => 'Country',
+        ];
+    }
+
+    /**
+     * @return \yii\db\ActiveQuery
+     */
+    public function getTeams()
+    {
+        return $this->hasMany(Team::className(), ['country_id' => 'id']);
+    }
+
+    /**
+     * @return \yii\db\ActiveQuery
+     */
+    public function getTournaments()
+    {
+        return $this->hasMany(Tournament::className(), ['country_id' => 'id']);
+    }
+}
